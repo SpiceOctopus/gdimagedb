9b6fce872300dc1812334f781214b85b7c4f1daa
Michael <vargson@outlook.com>
2024-11-02 21:52:00 +0100

    collection editor tiles now call new CacheManager function for thumbnails

--- src/ui/collections/collection_editor_tile/collection_editor_tile.gd
+++ src/ui/collections/collection_editor_tile/collection_editor_tile.gd
@@ -14,22 +14,10 @@ var collection_id : int
 @onready var button_right = $VBoxContainer/HBoxContainer/MoveRightButton
 
 func _ready() -> void:
-	if CacheManager.thumb_cache.has(media.id):
-		display.texture = CacheManager.thumb_cache[media.id]
-	else:
-		load_thread.start(Callable(self,"async_load"))
+	display.texture = CacheManager.get_thumbnail(media)
 	display.custom_minimum_size = Vector2(Settings.grid_image_size, Settings.grid_image_size)
 	custom_minimum_size = Vector2(Settings.grid_image_size, display.custom_minimum_size.y + $VBoxContainer/HBoxContainer.custom_minimum_size.y)
 	update_ui()
-
-func async_load() -> void:
-	var dir = DirAccess.open(OS.get_executable_path().get_base_dir())
-	if dir.file_exists(media.thumb_path):
-		var texture : ImageTexture = ImageUtil.TextureFromFile(media.thumb_path)
-		display.call_deferred("set_texture", texture)
-		CacheManager.call_deferred("add_thumbnail", media.id, texture)
-	elif media.path.get_extension() in Settings.supported_video_files:
-		display.call_deferred("set_texture", load("res://gfx/video_placeholder.png"))
 
 func _on_btn_move_left_pressed() -> void:
 	move_left.emit(media)
@@ -48,5 +36,5 @@ func update_ui() -> void:
 	
 	if media.position <= 0: # first one
 		button_left.disabled = true
-	elif media.position >= (DB.get_collection_count(collection_id) - 1): # last one
+	if media.position >= (DB.get_collection_count(collection_id) - 1): # last one
 		button_right.disabled = true
--- src/ui/media_properties/media_properties.gd
+++ src/ui/media_properties/media_properties.gd
@@ -9,11 +9,11 @@ func set_media(media_in : DBMedia) -> void:
 	$MarginContainer/VBoxContainer/IDContainer/IDValue.text = str(media.id)
 	path_value.text = media.path
 
-func _on_id_copy_pressed():
+func _on_id_copy_pressed() -> void:
 	DisplayServer.clipboard_set(str(media.id))
 
-func _on_path_copy_pressed():
+func _on_path_copy_pressed() -> void:
 	DisplayServer.clipboard_set(path_value.text)
 
-func _on_regenerate_thumb_button_pressed():
+func _on_regenerate_thumb_button_pressed() -> void:
 	ImportUtil.create_thumbnail(media.path)
